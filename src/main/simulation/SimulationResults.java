75166a044613db02ffd2d935925709061b3b7a72
pat-rosander <patrickrosander04@gmail.com>
2025-04-23 15:12:00 -0700

    Added SimulationResults.java

---
+++ src/main/simulation/SimulationResults.java
@@ -0,0 +1,9 @@
+package main.simulation;
+
+public class SimulationResults {
+    // Store winning hole cards
+    // Store winning hand
+    // Store each players hole cards
+    // Store each players best hand
+    // Store communityCards
+}
