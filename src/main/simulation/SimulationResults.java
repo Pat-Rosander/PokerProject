9039c35f336821d65dfa0a65a77fcad337fe76fc
pat-rosander <patrickrosander04@gmail.com>
2025-04-27 00:45:47 -0700

    Changed SimulationResults.java

--- src/main/simulation/SimulationResults.java
+++ src/main/simulation/SimulationResults.java
@@ -1,8 +1,13 @@
 package main.simulation;
+import java.util.ArrayList;
+import main.model.*;
 
 public class SimulationResults {
-    // Store winning hole cards
-    // Store winning hand
+    private ArrayList<Player> playersList;
+    private Player winningPlayer;
+    private ArrayList<Card> winningHoleCards;
+    private ArrayList<Card> winningHand;
+
     // Store each players hole cards
     // Store each players best hand
     // Store communityCards
